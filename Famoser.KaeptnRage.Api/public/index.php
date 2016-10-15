16ebd7949872c3d1df56edf40d6f379efc86a92f
florianalexandermoser <git@famoser.ch>
2016-10-15 14:13:40 +0200

    made it look nice

--- Famoser.KaeptnRage.Api/public/index.php
+++ Famoser.KaeptnRage.Api/public/index.php
@@ -16,50 +16,61 @@
 </head>
 <body>
 <header>
-    <h1>KaeptnRage</h1>
-    <aside>a crossplatform rage © solution</aside>
-</header>
-<section>
     <div class="container">
         <div class="row">
-            <div class="col-md-12">
-                <?php
-                $files = glob(__DIR__ . '/sounds/*.*');
-                $first = true;
-                foreach ($files as $file) {
-                    $filename = basename($file);
-                    $midPos = strrpos($filename, " - ");
-                    if ($midPos == -1)
-                        continue; //skip invalid filenames
-                    $author = substr($filename, 0, $midPos);
-                    $blockquote = substr($filename, $midPos + 3); ?>
-                    <?php if (!$first) echo "<hr>"; else $first = false; ?>
-
-                    <div class="sound">
-                        <audio src="sounds/<?= $filename ?>" controls></audio>
-                        <blockquote>
-                            <p><?= $blockquote ?></p>
-                            <small>by <cite><?= $author ?></cite></small>
-                        </blockquote>
-                    </div>
-                <?php } ?>
-
+            <div class="col-md-12 header">
+                <h1 id="title">KaeptnRage</h1>
+                <aside>a crossplatform rage © solution</aside>
+                <hr/>
             </div>
         </div>
     </div>
-</section>
+</header>
+<section>
+    <div class="container">
+        <div class="row content">
+            <?php
 
-<script>(function (i, s, o, g, r, a, m) {
-        i['GoogleAnalyticsObject'] = r;
-        i[r] = i[r] || function () {
-                (i[r].q = i[r].q || []).push(arguments)
-            }, i[r].l = 1 * new Date();
-        a = s.createElement(o), m = s.getElementsByTagName(o)[0];
-        a.async = 1;
-        a.src = g;
-        m.parentNode.insertBefore(a, m)
-    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
-    ga('create', 'UA-46163202-2', 'auto');
-    ga('send', 'pageview');</script>
+            function custom_decode($text)
+            {
+                $replace = array("ae" => "ä", "oe" => "ö", "ue" => "ü");
+                foreach ($replace as $key => $val) {
+                    $text = str_replace($key, $val, $text);
+                }
+                return $text;
+            }
+
+            $files = glob(__DIR__ . '/sounds/*.*');
+            $first = true;
+            foreach ($files as $file) {
+                $filename = basename($file);
+                $midPos = strrpos($filename, " - ");
+                if ($midPos == -1)
+                    continue; //skip invalid filenames
+                $author = substr($filename, 0, $midPos);
+                $blockquote = substr($filename, $midPos + 3);
+                $blockquote = substr($blockquote, 0, strrpos($blockquote, ".")); ?>
+
+                <div class="sound col-md-4">
+                    <div class="sound-inner">
+                        <audio src="sounds/<?= $filename ?>" controls></audio>
+                        <blockquote>
+                            <p>
+                                <?= custom_decode($blockquote) ?></p>
+                            <small>by <cite><?= custom_decode($author) ?></cite></small>
+                        </blockquote>
+                    </div>
+                </div>
+            <?php } ?>
+
+        </div>
+    </div>
+</section>
+<!--
+<script type="application/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script type="application/javascript" src="js/fittext.js"></script>
+
+<script>$("#title").fitText();</script>
+-->
 </body>
 </html>
---
+++ Famoser.KaeptnRage.Api/public/submit.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Florian Moser
+ * Date: 15.10.2016
+ * Time: 14:10
+ */
+
+echo "not implemented";
