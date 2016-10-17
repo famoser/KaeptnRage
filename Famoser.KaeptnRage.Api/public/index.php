a0a7ebf30daf5a16d8596b06a2fc67f790ae8964
Florian Moser <git@famoser.ch>
2016-10-17 22:35:03 +0200

    added sort

--- Famoser.KaeptnRage.Api/public/index.php
+++ Famoser.KaeptnRage.Api/public/index.php
@@ -1,3 +1,38 @@
+<?php
+
+function custom_decode($text)
+{
+    $replace = array("ae" => "ä", "oe" => "ö", "ue" => "ü");
+    foreach ($replace as $key => $val) {
+        $text = str_replace($key, $val, $text);
+    }
+    return $text;
+}
+
+$files = glob(__DIR__ . '/sounds/*.*');
+$first = true;
+$content = array();
+$authors = array();
+foreach ($files as $file) {
+    $localContent = array();
+    $filename = basename($file);
+    $midPos = strrpos($filename, " - ");
+    if ($midPos == -1)
+        continue; //skip invalid filenames
+    $author = substr($filename, 0, $midPos);
+    $blockquote = substr($filename, $midPos + 3);
+    $blockquote = substr($blockquote, 0, strrpos($blockquote, "."));
+
+    $localContent["author"] = $author;
+    $localContent["blockquote"] = $blockquote;
+    $localContent["filename"] = $filename;
+    $content[] = $localContent;
+
+    if (!in_array($author, $authors))
+        $authors[] = $author;
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -28,46 +63,55 @@
 </header>
 <section>
     <div class="container">
-        <div class="row content">
-            <?php
-
-            function custom_decode($text)
-            {
-                $replace = array("ae" => "ä", "oe" => "ö", "ue" => "ü");
-                foreach ($replace as $key => $val) {
-                    $text = str_replace($key, $val, $text);
-                }
-                return $text;
-            }
-
-            $files = glob(__DIR__ . '/sounds/*.*');
-            $first = true;
-            foreach ($files as $file) {
-                $filename = basename($file);
-                $midPos = strrpos($filename, " - ");
-                if ($midPos == -1)
-                    continue; //skip invalid filenames
-                $author = substr($filename, 0, $midPos);
-                $blockquote = substr($filename, $midPos + 3);
-                $blockquote = substr($blockquote, 0, strrpos($blockquote, ".")); ?>
-
-                <div class="sound col-md-4">
+        <div class="row">
+            <div class="col-md-12 select-buttons">
+                <button class="button select-button selected" data-filter="*">alli</button>
+                <?php foreach ($authors as $author) { ?>
+                    <button class="button select-button" data-filter=".<?= $author ?>"><?= $author ?></button>
+                <?php } ?>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row content sound-items">
+            <?php foreach ($content as $item) { ?>
+                <div class="sound col-md-4 <?= custom_decode($item["author"]) ?>">
                     <div class="sound-inner">
-                        <audio src="sounds/<?= $filename ?>" controls></audio>
+                        <audio src="sounds/<?= $item["filename"] ?>" controls></audio>
                         <blockquote>
                             <p>
-                                <?= custom_decode($blockquote) ?></p>
-                            <small>by <cite><?= custom_decode($author) ?></cite></small>
+                                <?= custom_decode($item["blockquote"]) ?></p>
+                            <small>by <cite><?= custom_decode($item["author"]) ?></cite></small>
                         </blockquote>
                     </div>
                 </div>
             <?php } ?>
-
         </div>
     </div>
 </section>
-<!--
 <script type="application/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="https://unpkg.com/isotope-layout@3.0/dist/isotope.pkgd.min.js"></script>
+<script type="application/javascript">
+    $isoGrid = $('.sound-items');
+    $isoGrid.isotope({
+        // set itemSelector so .grid-sizer is not used in layout
+        itemSelector: '.sound',
+        percentPosition: true,
+        layoutMode: 'masonry'
+        /*masonry: {
+         // use element for option
+         columnWidth: '.grid-sizer'
+         }*/
+    });
+
+    $(".select-button").on("click", function (e) {
+        e.preventDefault();
+        $(".select-button").removeClass("selected");
+        $(this).addClass("selected");
+        $isoGrid.isotope({filter: $(this).attr("data-filter")});
+    })
+</script>
+<!--
 <script type="application/javascript" src="js/fittext.js"></script>
 
 <script>$("#title").fitText();</script>
